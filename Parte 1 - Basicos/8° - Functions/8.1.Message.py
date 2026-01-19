9e61dbea87c7d657bca19738595e2d7701ba10c7
FiRS1247 <150846019+FiRS1247@users.noreply.github.com>
2026-01-18 18:59:19 -0600

    terminamos funciones

---
+++ Parte 1 - Basicos/8° - Functions/8.1.Message.py
@@ -0,0 +1,6 @@
+def display_message():
+    ingreso = input("Ingresa un nombre")
+    print(f"Hola {ingreso}")
+
+
+display_message()
---
+++ Parte 1 - Basicos/8° - Functions/8.2.Favorite_book.py
@@ -0,0 +1,6 @@
+def favorite_book(title):
+    print(f"My favorite book is {title.title()}")
+
+
+libro = input("Cual es tu libro favorito?")
+favorite_book(libro)
